3a350c0317b42cac42144fd2ba3ced17a4bcf3fb
hsa52 <hsa52@nau.edu>
2016-09-16 21:35:54 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -2,16 +2,25 @@
 #include <stdio.h>
 #include <ctype.h>
 
-void p6_to_p3(FILE*);
+void read_p6(FILE*,char,FILE*);
 void p3_to_p6(FILE*);
 void parse_white_space(FILE*);
+int get_number(FILE* fp);
+void write_p6(FILE *fp,char* data,int size);
+void write_p3(FILE *fp,char*data,int size);
 
-int main() {
+typedef struct Pixel {
+    unsigned char r, g, b;
+} Pixel;
+
+int main(int argc,char* argv[]) {
     FILE * fp ;
-    //fp= fopen("/Users/Hayden/ClionProjects/cs430_project1/boxes_1.ppm","r");
+    fp= fopen(argv[2],"r");
+    FILE* output=fopen(argv[3],"w+");
     //opening file
     if (fp==NULL){
-        perror("That file does not exist");
+        //printf("%s",argv[2]);
+        fprintf(stderr,"That file does not exist");
         exit(-1);
         //error checking file opening
     }
@@ -19,6 +28,7 @@ int main() {
         size_t result;
         int header_size=2;
         char header[header_size];
+        char* buffer;
         //creating buffer for checking what format the file is in
         result=fread(header,1,header_size,fp);
 
@@ -28,34 +38,27 @@ int main() {
             //error checking file reading
         }
         if(header[0]=='P'&&header[1]=='6'){
-            //printf("req");
-            p6_to_p3(fp);
+            printf("%c",*argv[1]);
+            read_p6(fp,*argv[1],output);
             //if p6 sending it to function for converting to p3
         }
         else if (header[0]=='P'&&header[1]=='3'){
-            //printf("req");
             p3_to_p6(fp);
             //if p3 sending it to function for converting to p6
         }
         else{
-            perror("Your file is not a PPM file");
+            fprintf(stderr,"This is not a ppm File");
             //if it is not a PPM file giving user an error message
         }
-
-
-
-
     }
-
     fclose(fp);
+    fclose(output);
 
     return 0;
 }
 
-void p6_to_p3(FILE * fp){
+void read_p6(FILE * fp,char formatto,FILE* output){
     int width,height,temp,max,temp2,i=0;
-
-
     if(fgetc(fp)=='#') {
         do temp = fgetc(fp); while (temp != '\n');
     }
@@ -63,41 +66,48 @@ void p6_to_p3(FILE * fp){
     fscanf(fp,"%i ",&height);
     fscanf(fp,"%i ",&width);
     fscanf(fp,"%i ",&max);
+    if(max>255){
+        fprintf(stderr,"This converter does no accept a max val of more than 255");
+    }
 
-    int buffer_size =width*height* sizeof(int);
-    printf("%i",width*height* sizeof(int));
 
-
-    int buffer[width*height* sizeof(int)];
     int f =ftell(fp);
     fseek(fp,-f,SEEK_CUR);
 
-
-    //printf("%c",*header);
-    //FILE* output=fopen("/Users/Hayden/ClionProjects/cs430_project1/output.ppm","w+");
-    int header[f];
-    fread(header, 1, f,fp);
-    fwrite(header,1, f,output);
+    int buffer_size =width*height*3+f;
+    char buffer[buffer_size];
 
 
-    //printf("%i",buffer_size);
+
     fread(buffer,1,buffer_size,fp);
-    fwrite(buffer,1,buffer_size,output);
-    //fprintf(, 1, buffer_size, output);
+
+    if(formatto=='6'){
+        printf("na");
+        write_p6(output,buffer,buffer_size);
+    }
+    if(formatto=='3'){
+        printf("na3");
+        write_p3(output,buffer,buffer_size);
+    }
 
 
-    //int temp = fgetc(fp);
-    //printf("%c",temp);
 
 
 
+}
+void write_p6(FILE *fp,char* data,int size){
+    fwrite(data,1,size,fp);
+    fseek(fp,1,SEEK_SET);
+    fwrite("6",1,1,fp);
+}
+void write_p3(FILE *fp,char*data,int size){
+    fprintf(fp,"%s",data);
+    fseek(fp,1,SEEK_SET);
 
-
-
-    //nothing yet
 }
 
 void p3_to_p6(FILE * fp){
+
     int width,height,temp,max,temp2,i=0;
     parse_white_space(fp);
 
@@ -108,46 +118,30 @@ void p3_to_p6(FILE * fp){
     fscanf(fp,"%i ",&height);
     fscanf(fp,"%i ",&width);
     fscanf(fp,"%i ",&max);
-    printf("%i",max);
-    int buffer[width*height];
-
-
-    int f =ftell(fp);
-    fseek(fp,-f,SEEK_CUR);
-    int header[f];
-    fread(header, 1, f,fp);
-    //printf("%c",*header);
-    //FILE* output=fopen("/Users/Hayden/ClionProjects/cs430_project1output.ppm","w+");
-    fwrite(header,1, f,output);
-    int u =0;
-    while((temp2=fgetc(fp))!=EOF){
-
-
-
+    if(max>255){
+        fprintf(stderr,"This converter does no accept a max val of more than 255");
     }
 
-    fwrite(buffer,1,height*width,output);
-
-    fclose(output);
-
-    //fgets(buffer,(height*width)-f,fp);
-    //fread(buffer,1,width*height,fp);
-    //atoi(buffer);
-    // fwrite(buffer,1,width*height,output);
-
-
-/*
-    do{
-        temp2=fgetc(fp);
-        buffer[i++]=temp2;
-
-    } while(temp2!=' ');
-    fwrite(buffer,1,width*height,output);
-    */
 
 
 
+    Pixel pix[height*width];
+    int buffer_size=height*width*3;
 
+    //printf("%i",max);
+    char buffer[width*height*3];
+
+
+
+    for(int i =0;i<buffer_size;i++){
+        buffer[i]=temp;
+        pix[i].r=get_number(fp);
+        parse_white_space(fp);
+        pix[i].g=get_number(fp);
+        parse_white_space(fp);
+        pix[i].b=get_number(fp);
+        parse_white_space(fp);
+    }
 
 
 
@@ -156,40 +150,14 @@ void p3_to_p6(FILE * fp){
 
 
 
-    //
-
-
-    //fseek(fp,0,SEEK_END);
-    //   int a = ftell(fp);
-    //  printf("%i",a);
-    // fsetpos(fp,f);
-    // fread(buffer,1,a-f,fp);
-    // fwrite(buffer,1,a-f,output);
-
-
-
-
-    //fseek(output,1,SEEK_SET);
-    //fwrite("6",1,1,output);
-
-
-    // *buffer=fgetc(fp);
-    //temp2 =fgetc(fp);
-    //   printf("%c %i",header[2], f);
-
-
-
-
-
-
-
-
-
-
+}
+int get_number(FILE* fp){
+    int number;
+    fscanf(fp,"%i",&number);
+    return number;
 }
 void parse_white_space(FILE *fp){
     while (fgetc(fp)==' '){
         fseek(fp,1,SEEK_CUR);
     }
 }
-
